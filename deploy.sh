9ea8f5a824ccdac33ea920860734d6e345066283
PhanDuck <lupikario0507@gmail.com>
2025-10-22 12:07:13 +0700

    Hoàn thành deploy front-end

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-# 1. Build ứng dụng (Tạo thư mục 'dist')
 echo "Building app..."
 npm run build
 
-# 2. Deploy các tệp đã build lên server
-# THAY THẾ 'your_key_name.pem' BẰNG TÊN FILE KHÓA CỦA BẠN
 echo "Deploying files to server..."
 scp -r -i ./swp-key.pem dist/* root@103.200.20.190:/var/www/html/
 
